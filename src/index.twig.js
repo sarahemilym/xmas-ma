7aca6ecb1211bd658ffbdba5d40a44c7e092c069
Sarah Mendham <sarah.mendham@bynd.com>
2018-11-15 17:02:41 -0800

    Tweaks to header and adding banner on homepage

--- src/index.twig.js
+++ src/index.twig.js
@@ -1,6 +1,8 @@
 import template from './index.twig';
 import burgerMenu from './assets/svgs/burger-menu.svg';
+import close from './assets/svgs/close.svg';
 
 export default template({
   burgerMenu,
+  close
 });
--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -1,32 +1,67 @@
+import { runInThisContext } from "vm";
+
 class Header {
   constructor() {
     this.header = document.getElementsByClassName('header')[0];
     this.burgerMenu = document.getElementsByClassName('header__btn')[0];
     this.nav = document.getElementsByClassName('header__nav')[0];
+    this.banner = document.getElementsByClassName('base-strip')[0];
     this.addListeners();
+
+    if (this.banner != undefined) {
+      this.adjustBanner();
+    }
   }
 
   addListeners() {
     this.burgerMenu.addEventListener('click', this.toggleBurgerMenu.bind(this));
     window.addEventListener('scroll', this.fixNav.bind(this));
+    window.addEventListener('resize', this.adjustBanner.bind(this));
   }
 
   toggleBurgerMenu() {
     if (this.nav.classList.contains('header__nav--open')){
+      this.header.classList.remove('header--bkgd');
       this.nav.classList.remove('header__nav--open');
+      this.header.classList.remove('header--open');
     } else {
+      this.header.classList.add('header--bkgd');
       this.nav.classList.add('header__nav--open');
+      this.header.classList.add('header--open');
     }
   }
 
   fixNav(event) {
     const navPos = this.nav.getBoundingClientRect().top;
-    if (navPos <= 26) {
+    console.log('nav pos', navPos);
+    if (navPos <= 59 && navPos > 26) {
+      this.nav.classList.add('header__nav-transition');
+    } else {
+      this.nav.classList.remove('header__nav-transition');
+    }
+    if (navPos <= 26 || window.innerWidth < 768) {
       this.nav.classList.add('header__nav--fixed');
-    } if (this.nav.classList.contains('header__nav--fixed') && window.pageYOffset <= 57) {
+      this.header.classList.add('header--bkgd');
+    } if (window.pageYOffset <= 57) {
       this.nav.classList.remove('header__nav--fixed');
+      this.header.classList.remove('header--bkgd');
     }
   }
+
+  adjustBanner() {
+    if (window.innerHeight < 730) {
+      this.banner.style.display = 'none';
+    } else {
+      const bannerTop = this.banner.getBoundingClientRect().top;
+      const bannerBtm = window.innerHeight - 50;
+
+      this.banner.style.height = bannerBtm - bannerTop + 'px';
+      this.banner.style.display = 'block';
+    }
+
+
+  }
+
 }
 
 export default Header;
